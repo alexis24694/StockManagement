8ca3d1ddfcf8041e049f2d26433d3d2c1d1a3082
Alexis Leon <a20111836@pucp.pe>
2019-05-21 16:50:22 -0500

    first commit

---
+++ StockManagement/Controllers/ItemController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StockManagement.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddItem()
+        {
+            return PartialView("AddItem");
+        }
+
+        public ActionResult ShowItems()
+        {
+            return PartialView("ShowItems");
+        }
+
+        public ActionResult EditItem()
+        {
+            return PartialView("EditItem");
+        }
+
+        public ActionResult DeleteItem()
+        {
+            return PartialView("DeleteItem");
+        }
+    }
+}

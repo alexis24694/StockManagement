94de0d71328e75b9b04e395da6194f4a829ee5e6
Alexis Leon <a20111836@pucp.pe>
2019-05-22 10:13:41 -0500

    angularjs file structure

---
+++ StockManagement/app/EditItemController.js
@@ -0,0 +1,39 @@
+﻿app.controller("EditItemController", function ($scope, $location,ShareData,StockManagementService) {
+     
+    getItem();
+    function getItem() {
+        
+        var promiseGetItem = StockManagementService.getItem(ShareData.value);
+
+        
+        promiseGetItem.then(function (pl)
+        {
+            $scope.Item = pl.data;
+        },
+              function (errorPl) {
+                  $scope.error = 'Error loading Item', errorPl;
+              });
+    }
+
+
+    //The Save method used to make HTTP PUT call to the WEB API to update the record
+
+    $scope.save = function () {
+        var Item = {
+            Code: $scope.Item.Code,
+            Name: $scope.Item.Name,
+            Description: $scope.Item.Description,
+            Quantity: $scope.Item.Quantity
+        };
+
+        var promisePutItem = StockManagementService.put($scope.Item.Id,Item);
+        promisePutItem.then(function (pl)
+        {
+            $location.path("/showitem");
+        },
+              function (errorPl) {
+                  $scope.error = 'Error loading Item', errorPl;
+              });
+    };
+
+});
